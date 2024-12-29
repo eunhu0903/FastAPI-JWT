7f7228d3a9765b65ce2396ad10b1edf34fa61b54
은후 <eunhoo0903@naver.com>
2024-12-29 21:29:12 +0900

    테이블 생성이 안되는 오류 해결

--- app/db/database.py
+++ app/db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from core.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
